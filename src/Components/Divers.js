e725044e5a4eb297b6c966f3626e6bed58f54ef4
said.T <tarhisaid@gmail.com>
2021-07-25 01:31:38 +0100

    React CV CREATOR
    STYLING SAVING 2

--- src/Components/Divers.js
+++ src/Components/Divers.js
@@ -1,6 +1,29 @@
 function Divers(){
     return(
+        <>
         <div className="divers">Détails personnels</div>
+        <div className="coordonnees">
+            <label htmlFor="nom"> Nom </label>
+            <input type="text" id="nom"></input>
+            <label htmlFor="prenom"> prenom </label>
+            <input type="text" id="prenom"></input>
+            <label htmlFor="tel"> tel </label>
+            <input type="tel" id="tel"></input>
+            <label htmlFor="adress"> adress </label>
+            <input type="text" id="adress"></input>
+            <label htmlFor="Ville"> ville </label>
+            <input type="text" id="Ville"></input>
+            <label htmlFor="pays"> pays </label>
+            <input type="text" id="pays"></input>
+            <label htmlFor="dateDeNaissance"> dateDeNaissance </label>
+            <input type="date" id="dateDeNaissance"></input>
+            <label htmlFor="email"> email </label>
+            <input type="email" id="email"></input>
+            
+
+
+        </div>
+        </>
     );
 }
 
--- src/Components/Entete.js
+++ src/Components/Entete.js
@@ -8,28 +8,6 @@ function Entete(){
 
     return(
         <div className="Entete">
-        <div className="coordonnees">
-            <label htmlFor="nom"> Nom </label>
-            <input type="text" id="nom"></input>
-            <label htmlFor="prenom"> prenom </label>
-            <input type="text" id="prenom"></input>
-            <label htmlFor="tel"> tel </label>
-            <input type="tel" id="tel"></input>
-            <label htmlFor="adress"> adress </label>
-            <input type="text" id="adress"></input>
-            <label htmlFor="Ville"> ville </label>
-            <input type="text" id="Ville"></input>
-            <label htmlFor="pays"> pays </label>
-            <input type="text" id="pays"></input>
-            <label htmlFor="dateDeNaissance"> dateDeNaissance </label>
-            <input type="date" id="dateDeNaissance"></input>
-            <label htmlFor="email"> email </label>
-            <input type="email" id="email"></input>
-            
-
-
-        </div>
-        <TextareaAutosize maxRows="5" className="titreCV" defaultValue="Titre"></TextareaAutosize>
         <div>
             <input id="image" type="file" className="image" onChange={(e)=>{
                 let reader = new FileReader();
@@ -40,8 +18,10 @@ function Entete(){
                 reader.readAsDataURL(e.target.files[0]);
                 
             }}/>
-            <img src={myPreview} alt="" style={{width:"200px",height:"200px"}}/>
+            <img src={myPreview} alt="" style={{width:"210px",height:"210px",borderRadius:"100%"}}/>
         </div>
+        <TextareaAutosize maxRows="5" className="titreCV" defaultValue="Titre"></TextareaAutosize>
+        
         </div>
 
     );
